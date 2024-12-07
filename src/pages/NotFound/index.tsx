b7bd77681dbb5c36afc0ce3bf1275127e7ceec62
kaiyou <yhy.mimimimimi@gmail.com>
2024-12-08 00:02:50 +0900

    style(notfound page): add style for notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './index.module.scss';
 
-const NotFound: React.FC = () => <div className="page-box">404 - Page Not Found</div>;
+const NotFound: React.FC = () => (
+  <div className={`page-box ${styles.pageNotFound}`}>
+    <main className={styles.container}>
+      <h1 className={styles.title}>Not Found - 404</h1>
+      <p>An error has occurred, to continue:</p>
+      <p>
+        Return to our <Link to="/">homepage</Link>.
+      </p>
+    </main>
+  </div>
+);
 
 export default NotFound;
